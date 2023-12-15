2b502ec65cdb2a429740efd644b5678d64988243
Adrianne <96875139+AdrianneC@users.noreply.github.com>
2023-12-15 20:33:08 +0000

    Included JavaScript code to pull in city detail

--- starter/script.js
+++ starter/script.js
@@ -1,129 +1,33 @@
 
-function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-  
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    var queryParams = { "api-key": "66ac4396710bd690ceec92a550419151" };
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-input")
-      .val()
-      .trim();
-  
-    
-  
-    // Logging the URL so we have access to it for troubleshooting
-    console.log("---------------\nURL: " + queryURL + "\n---------------");
-    console.log(queryURL + $.param(queryParams));
-    return queryURL + $.param(queryParams);
-  }
-  
-  /**
-   * takes API data (JSON/object) and turns it into elements on the page
-   * @param {object} NYTData - object containing NYT API data
-   */
-  function updatePage(NYTData) {
-    // Get from the form the number of results to display
-    // API doesn't have a "limit" parameter, so we have to do this ourselves
-    var numArticles = $("#article-count").val();
-  
-    // Log the NYTData to console, where it will show up as an object
-    console.log(NYTData);
-    console.log("------------------------------------");
-  
-    // Loop through and build elements for the defined number of articles
-    for (var i = 0; i < numArticles; i++) {
-      // Get specific article info for current index
-      var article = NYTData.response.docs[i];
-  
-      // Increase the articleCount (track article # - starting at 1)
-      var articleCount = i + 1;
-  
-      // Create the  list group to contain the articles and add the article content for each
-      var $articleList = $("<ul>");
-      $articleList.addClass("list-group");
-  
-      // Add the newly created element to the DOM
-      $("#article-section").append($articleList);
-  
-      // If the article has a headline, log and append to $articleList
-      var headline = article.headline;
-      var $articleListItem = $("<li class='list-group-item articleHeadline'>");
-  
-      if (headline && headline.main) {
-        console.log(headline.main);
-        $articleListItem.append(
-          "<span class='label label-primary'>" +
-          articleCount +
-          "</span>" +
-          "<h2> " +
-          headline.main +
-          "</h2>"
-        );
-      }
-  
-      // If the article has a byline, log and append to $articleList
-      var byline = article.byline;
-  
-      if (byline && byline.original) {
-        console.log(byline.original);
-        $articleListItem.append("<h3>" + byline.original + "</h3>");
-      }
-  
-      // Log section, and append to document if exists
-      var section = article.section_name;
-      console.log(article.section_name);
-      if (section) {
-        $articleListItem.append("<h5>Section: " + section + "</h5>");
-      }
-  
-      // Log published date, and append to document if exists
-      var pubDate = article.pub_date;
-      console.log(article.pub_date);
-      if (pubDate) {
-        $articleListItem.append("<h5>" + article.pub_date + "</h5>");
-      }
-  
-      // Append and log url
-      $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
-      console.log(article.web_url);
-  
-      // Append the article
-      $articleList.append($articleListItem);
-    }
-  }
-  
-  // Function to empty out the articles
-  function clear() {
-    $("#article-section").empty();
-  }
-  
-  // CLICK HANDLERS
-  // ==========================================================
-  
-  // .on("click") function associated with the Search Button
-  $("#run-search").on("click", function (event) {
-    // This line allows us to take advantage of the HTML "submit" property
-    // This way we can hit enter on the keyboard and it registers the search
-    // (in addition to clicks). Prevents the page from reloading on form submit.
+// This is our API key
+var APIKey = "66ac4396710bd690ceec92a550419151";
+
+
+// This .on("click") function will trigger the fetch() Call
+$("#search-button").on("click", function (event) {
+
+    // event.preventDefault() can be used to prevent an event's default behavior.
+    // Here, it prevents the submit button from trying to submit a form when clicked
     event.preventDefault();
   
-    // Empty the region associated with the articles
-    clear();
+    // Here we grab the text from the input box
+    var city = $("#search-input").val();
   
-    // Build the query URL for the Fetch request to the NYT API
-    var queryURL = buildQueryURL();
+    // Here we construct our URL
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
   
-    // Make the Fetch request to the API - GETs the JSON data at the queryURL.
-    // The data then gets passed as an argument to the updatePage function
     fetch(queryURL)
       .then(function (response) {
         return response.json();
       })
-      .then(updatePage);
-  });
+      .then(function (data) {
+
+
+        $("#forecast").text(JSON.stringify(data));
+
+
+
+      });
   
-  //  .on("click") function associated with the clear button
-  $("#clear-all").on("click", clear);
+
+});
